ed3bb5d95fdfc557fdae7818b5377f0308173f02
Elias0419 <108772953+Elias0419@users.noreply.github.com>
2024-09-25 21:58:49 -0400

    refactoring

--- automated_arch_install.sh
+++ automated_arch_install.sh
@@ -7,14 +7,14 @@
 # Syslinux is used for booting because its simple and I like it
 # This script is tailored specifically for my point of sale, but could be modified for other simple kiosk installations
 
-# Warning: Running this script with breakpoints disabled will erase /dev/sda without warning
+# Warning: Running this script as-is will erase /dev/sda without warning. Make sure you know what you're doing.
 
 
 # Configuration Options
 
-ETHERNET=0          # Flag whether Ethernet connectivity will be used during install, 1 for true
+ETHERNET=0          # Use Ethernet during install
 #EFI=0               # Not implemented
-STATIC=0            # Flag for whether static IP should be used for the wifi connection, 1 for true
+STATIC=0            # Flag for whether static IP should be used for the wifi connection
 STATIC_IP="123.456.789.0" # The static IP address to assign if STATIC is set to 1
 WIFI_SSID="XYZ"    # The SSID of the wifi network to connect to
 WIFI_PASSWORD="xyz" # The password for the wifi network
@@ -30,12 +30,7 @@ SSH_PASSWORD="x"           # The password to set for SSH access during installat
 SERVICE_FILE="/mnt/etc/systemd/system/installer_run_once.service"
 HAS_RUN="/has_run"
 
-BREAKPOINTS=0 # Set to 1 to step through the script, 0 for fully automated
 
-# if [ "$BREAKPOINTS" -eq 0 ]; then
-#     read -p "WARNING: We are in fully automatic mode! If you don't know why you're here or what you're doing, say no! Continue? (yes/no): " confirm
-#     [[ $confirm != "yes" ]] && exit 1
-# fi
 
 
 breakpoint() {
@@ -52,123 +47,107 @@ breakpoint() {
 
 create_partitions() {
 
-        local device=$TARGET_DISK
-        # this is 2 gigs for /boot.
-        local boot_part=$((2*1024*1024*1024))
-        local total_size_bytes=$(cat /sys/block/${device##*/}/size)
-        local sector_size=$(cat /sys/block/${device##*/}/queue/hw_sector_size)
-        local total_size=$((total_size_bytes*sector_size))
-        local remaining_size=$((total_size - boot_part))
+    local device=$TARGET_DISK
+    # this is 2 gigs for /boot.
+    local boot_part=$((2*1024*1024*1024))
+    local total_size_bytes=$(cat /sys/block/${device##*/}/size)
+    local sector_size=$(cat /sys/block/${device##*/}/queue/hw_sector_size)
+    local total_size=$((total_size_bytes*sector_size))
+    local remaining_size=$((total_size - boot_part))
 
-        local half_remaining=$((remaining_size / 2))
-        local boot_part_sectors=$((boot_part / sector_size))
-        local half_remaining_sectors=$((half_remaining / sector_size))
+    local half_remaining=$((remaining_size / 2))
+    local boot_part_sectors=$((boot_part / sector_size))
+    local half_remaining_sectors=$((half_remaining / sector_size))
 
-        (
-        echo ",${boot_part_sectors},L"
-        echo ",${half_remaining_sectors},L"
-        echo ",,L"
-        ) | sfdisk $device # write the partitions
+    (
+    echo ",${boot_part_sectors},L"
+    echo ",${half_remaining_sectors},L"
+    echo ",,L"
+    ) | sfdisk $device # write the partitions
 
 }
+format_partitions() {
+    mkfs.ext4 -F /dev/sda1
+    mkfs.ext4 -F /dev/sda2
+    mkfs.ext4 -F /dev/sda3
+}
 
-# check if we've run before
-if mount /dev/sda2 /mnt; then
 
-    if [ -f /mnt"$HAS_RUN" ]; then
-        echo "Installation already completed."
-        echo "Please press ENTER to reboot and remove the installation medium."
-        read -p ""
+mount_partitions() {
+    mount /dev/sda2 /mnt
+    mkdir /mnt/boot
+    mkdir /mnt/home
+    mount /dev/sda1 /mnt/boot
+    mount /dev/sda3 /mnt/home
+}
+
+install_base_system() {
+    yes | pacman-key --init
+    yes | pacman-key --populate archlinux
+    pacstrap /mnt base linux linux-firmware xorg networkmanager xorg-server sddm lxqt breeze-icons syslinux gptfdisk sudo xorg-xinit
+}
+
+write_fstab() {
+    echo "/dev/sda1 /boot ext4 defaults 0 2" >> /mnt/etc/fstab
+    echo "/dev/sda2 / ext4 defaults 0 1" >> /mnt/etc/fstab
+    echo "/dev/sda3 /home ext4 defaults 0 2" >> /mnt/etc/fstab
+}
+
+check_first_run() {
+    if mount /dev/sda2 /mnt; then
+
+        if [ -f /mnt"$HAS_RUN" ]; then
+            echo "Installation already completed."
+            echo "Please press ENTER to reboot and remove the installation medium."
+            read -p ""
+            umount /mnt
+            reboot
+            exit 0
+        fi
         umount /mnt
-        reboot
-        exit 0
+
     fi
-    umount /mnt
+}
 
-fi
+check_ssh() {
+    if [ "$TOGGLE_SSH" -eq 1 ]; then
+        echo -e "$SSH_PASSWORD\n$SSH_PASSWORD" | passwd
+    fi
+}
 
-# if we need ssh during install we need to set a root password
-# (this is only for the installer, the installed system will use $ROOT_PASSWORD)
-if [ "$TOGGLE_SSH" -eq 1 ]; then
-    echo -e "$SSH_PASSWORD\n$SSH_PASSWORD" | passwd
-fi
+connect_to_the_internet() {
+    if [ "$ETHERNET" -eq 1 ]; then
+        # Ethernet probably "just works"
+        echo ""
+    else
+        # Detect wifi adapter. The regex seems pretty reliable, but I haven't tested it thoroughly
+        wifi_adapter=$(ip addr | grep -Eo 'wlan[0-9]|wlp[0-9]s[0-9]|wlx[[:xdigit:]]{12}' | head -n 1)
 
-# connect to the internet
-if [ "$ETHERNET" -eq 1 ]; then
-    # Ethernet probably "just works"
-    echo ""
-else
-    # Detect wifi adapter. The regex seems pretty reliable, but I haven't tested it thoroughly
-    wifi_adapter=$(ip addr | grep -Eo 'wlan[0-9]|wlp[0-9]s[0-9]|wlx[[:xdigit:]]{12}' | head -n 1)
+        ssid=$WIFI_SSID
+        wifi_password=$WIFI_PASSWORD
 
-    ssid=$WIFI_SSID
-    wifi_password=$WIFI_PASSWORD
+        systemctl start NetworkManager.service
+        sleep 5
 
-    systemctl start NetworkManager.service
-    sleep 5
+        nmcli device wifi rescan
+        sleep 5
 
-    nmcli device wifi rescan
-    sleep 5
-
-    nmcli dev wifi connect "$ssid" password "$wifi_password" ifname "$wifi_adapter"
-fi
+        nmcli dev wifi connect "$ssid" password "$wifi_password" ifname "$wifi_adapter"
+    fi
 
 
-if [ "$STATIC" -eq 1 ]; then
-    nmcli con mod "$ssid" ipv4.addresses "$STATIC_IP"/24 ipv4.method manual
+    if [ "$STATIC" -eq 1 ]; then
+        nmcli con mod "$ssid" ipv4.addresses "$STATIC_IP"/24 ipv4.method manual
 
-    systemctl restart NetworkManager.service
-    sleep 5
-    nmcli con up "$ssid"
-fi
+        systemctl restart NetworkManager.service
+        sleep 5
+        nmcli con up "$ssid"
+    fi
+    # get the mac address
+    mac_address=$(ip link show $wifi_adapter | grep -Po 'link/ether \K[^ ]+')
 
-if [ "$BREAKPOINTS" -eq 1 ]; then
- breakpoint "At this point we are connected to the internet and ssh is ready. Exit the script now (q) to do a manual installation or press c to continue"
-fi
-
-if [ "$BREAKPOINTS" -eq 1 ]; then
- breakpoint "We're about to wipe $TARGET_DISK! Press c to continue or q to cancel."
-fi
-
-# create partitions
-create_partitions
-
-# format partitions
-mkfs.ext4 -F /dev/sda1
-mkfs.ext4 -F /dev/sda2
-mkfs.ext4 -F /dev/sda3
-
-# mount partitions
-mount /dev/sda2 /mnt
-mkdir /mnt/boot
-mkdir /mnt/home
-mount /dev/sda1 /mnt/boot
-mount /dev/sda3 /mnt/home
-
-# install the system
-yes | pacman-key --init
-yes | pacman-key --populate archlinux
-pacstrap /mnt base linux linux-firmware xorg networkmanager xorg-server sddm lxqt breeze-icons syslinux gptfdisk sudo xorg-xinit
-
-# fstab is written manually because genfstab is not reliable
-echo "/dev/sda1 /boot ext4 defaults 0 2" >> /mnt/etc/fstab
-echo "/dev/sda2 / ext4 defaults 0 1" >> /mnt/etc/fstab
-echo "/dev/sda3 /home ext4 defaults 0 2" >> /mnt/etc/fstab
-
-# hostname and timezone
-echo $HOSTNAME > /mnt/etc/hostname
-ln -sf /usr/share/zoneinfo/$TIMEZONE /mnt/etc/localtime
-
-if [ "$BREAKPOINTS" -eq 1 ]; then
- breakpoint "At this point the system is installed, without a bootloader or basic configuration. The next part does those things. Exit now (q) to do this part manually or c to continue. "
-fi
-
-# get the mac address
-mac_address=$(ip link show $wifi_adapter | grep -Po 'link/ether \K[^ ]+')
-
-# copy network stuff to the installed system
-mkdir -p /mnt/etc/NetworkManager/system-connections
-
+    # copy network stuff to the installed system
+    mkdir -p /mnt/etc/NetworkManager/system-connections
     cat <<EOF > /mnt/etc/NetworkManager/system-connections/$ssid.nmconnection
 [connection]
 id=$ssid
@@ -191,13 +170,18 @@ method=auto
 method=auto
 EOF
 
-
-
 chmod 600 /mnt/etc/NetworkManager/system-connections/$ssid.nmconnection
+}
 
-# set up the rigs installer to run on the next boot
-cp /root/rigs_pos_installer.sh /mnt/home
-chmod +x /mnt/home/rigs_pos_installer.sh
+write_hostname_and_timezone() {
+
+    echo $HOSTNAME > /mnt/etc/hostname
+    ln -sf /usr/share/zoneinfo/$TIMEZONE /mnt/etc/localtime
+}
+
+write_rigs_installer() {
+    cp /root/rigs_pos_installer.sh /mnt/home
+    chmod +x /mnt/home/rigs_pos_installer.sh
     cat <<EOF > $SERVICE_FILE
 [Unit]
 Description=Run Once Installation Task
@@ -214,8 +198,9 @@ RemainAfterExit=yes
 [Install]
 WantedBy=multi-user.target
 EOF
+}
 
-# main configuation of the installed system
+configure_the_system() {
 arch-chroot /mnt /bin/bash <<EOF
 hwclock --systohc # set hardware clock
 echo "LANG=$LOCALE" > /etc/locale.conf                      # add locale to locale.conf
@@ -238,10 +223,19 @@ echo -e "[Service]\nExecStart=\nExecStart=-/usr/bin/agetty --autologin $USER_NAM
 systemctl enable NetworkManager.service # enable networkmanager so we have a connection on boot
 systemctl enable installer_run_once.service # enable the installer to run on next boot
 EOF
-if [ "$BREAKPOINTS" -eq 1 ]; then
-    breakpoint "Installation is complete, continue (c) to reboot or cancel (q) for further manual configuration."
-fi
+}
 
+###
+check_first_run
+create_partitions
+format_partitions
+mount_partitions
+check_ssh
+connect_to_the_internet
+install_base_system
+write_fstab
+write_hostname_and_timezone
+write_rigs_installer
+configure_the_system
 touch /mnt"$HAS_RUN"
-
 reboot
